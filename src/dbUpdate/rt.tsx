55b1236281a6d1a42c538cf190f99085680e762d
liulei11 <liulei11@kuaishou.com>
2023-02-15 21:52:42 +0800

    feat: decode

--- src/dbUpdate/rt.tsx
+++ src/dbUpdate/rt.tsx
@@ -1,6 +1,8 @@
+import {safeDecodeURIComponent} from "../_utils/util";
+
 export default function ({env, data, outputs, inputs, onError}) {
   inputs['params']((val, relOutpus) => {
-	  let script = data.rules?.script;
+	  let script = safeDecodeURIComponent(data.rules?.script);
 		
 	  if (script) {
 		  const sql = eval(script)(val)
