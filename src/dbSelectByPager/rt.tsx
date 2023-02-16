fbfe53ec6f491f0531aaf4b0858c957a9ff6b780
liulei11 <liulei11@kuaishou.com>
2023-02-16 21:52:21 +0800

    feat: 串行请求

--- src/dbSelectByPager/rt.tsx
+++ src/dbSelectByPager/rt.tsx
@@ -1,4 +1,5 @@
 import {safeDecodeURIComponent} from "../_utils/util";
+import {AnyType} from "../_types";
 
 export default function ({env, data, outputs, inputs, onError}) {
   let script = data.selector?.script;
@@ -23,23 +24,18 @@ export default function ({env, data, outputs, inputs, onError}) {
 	      });
     }
   } else {
-	  inputs['params']((val, outputRels) => {
+	  inputs['params'](async (val, outputRels) => {
 		  const values = { ...(val.pageParams || {}), ...(val.params || {}) };
 			
 		  if (script.list) {
-			  Promise.all([
-				  eval(safeDecodeURIComponent(script.list))(values, env.executeSql),
-				  eval(safeDecodeURIComponent(script.total))(values, env.executeSql)
-			  ])
-			  .then(([data, countData]) => {
-				  outputs['rtn']({
-					  list: data,
-					  total: countData[0]?.total
-				  });
-			  })
-			  .catch(ex => {
-				  onError(`执行SQL发生错误,${ex?.message}`);
-			  });
+				try {
+					const data = await eval(safeDecodeURIComponent(script.list))(values, env.executeSql);
+					const countData = await eval(safeDecodeURIComponent(script.total))(values, env.executeSql);
+					
+					outputs['rtn']({ list: data, total: countData[0]?.total });
+				} catch (e: AnyType) {
+					onError(`执行SQL发生错误,${e?.message}`);
+				}
 		  }
 	  })
 	}
