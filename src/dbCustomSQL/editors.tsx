e993b376e26866d1d8e7f2ca7855f5812df368bc
liulei11 <liulei11@kuaishou.com>
2023-05-09 17:03:38 +0800

    feat: 初始化自定义 SQL 组件

---
+++ src/dbCustomSQL/editors.tsx
@@ -0,0 +1,54 @@
+/**
+ * MyBricks Opensource
+ * https://mybricks.world
+ * This source code is licensed under the MIT license.
+ *
+ * CheMingjun @2019
+ * mybricks@126.com
+ */
+import {validateEntity} from "../_utils/validate";
+
+export default {
+	/** 环境发生变化 */
+	'@envChanged'({ data, env, cancelError, throwError }) {
+		const error = validateEntity(data.rules.entities, env.entity);
+		
+		error ? throwError(error) : cancelError();
+		data.errorMessage = error;
+	},
+  ':root': [
+    {
+      title: '编辑',
+      type: 'domain.dbInsert',
+      options({data, input, output}) {
+        return {
+          get paramSchema() {
+            return input.get('params').schema || {};
+          },
+          get errorMessage() {
+            return data.errorMessage;
+          },
+        }
+      },
+      value: {
+        get({data, input, output}) {
+          return data.rules;
+        },
+        set({data, setDesc, outputs, cancelError}, val) {
+          data.rules = val;
+	        data.errorMessage = '';
+	        cancelError();
+
+          if (data.rules) {
+            setDesc(`已选择 ${data.rules.desc}`);
+          } else {
+            setDesc(`未完成选择`);
+          }
+        }
+      }
+    }
+  ]
+}
+
+
+
---
+++ src/dbCustomSQL/rt.tsx
@@ -0,0 +1,19 @@
+import {safeDecodeURIComponent} from "../_utils/util";
+
+export default function ({env, data, outputs, inputs, onError}) {
+  inputs['params']((val, relOutpus) => {
+	  let script = safeDecodeURIComponent(data.rules?.script);
+		
+	  if (script) {
+		  try {
+			  const sql = eval(script)(val, env)
+			  env.executeSql(sql)
+			    .then(data => outputs['rtn'](data.insertId || data.rows?.[0]?.insertId))
+			    .catch(ex => onError(`执行SQL发生错误, ${ex?.message}`));
+		  } catch (error: any) {
+			  env.edit ? console.error('执行SQL发生错误, ', error) : undefined;
+			  onError(`执行错误, ${error?.message}`);
+		  }
+    }
+  })
+}
