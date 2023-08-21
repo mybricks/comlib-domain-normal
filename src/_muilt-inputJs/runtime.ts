7ac7dc8c5efe4d76190a9f6e2e48b1ec4eca5da5
liulei11 <liulei11@kuaishou.com>
2023-08-21 11:48:07 +0800

    feat: js 计算增加 env

--- src/_muilt-inputJs/runtime.ts
+++ src/_muilt-inputJs/runtime.ts
@@ -1,34 +1,37 @@
 import { Data } from './constants';
 import { convertObject2Array } from './util';
+import { AnyType } from '../_types';
 
 export default function ({ env, data, inputs, outputs, onError }: RuntimeParams<Data>) {
-  const { fns, runImmediate } = data;
-  const isDebug = env.runtime?.debug;
+	const { fns, runImmediate } = data;
+	const isDebug = env.runtime?.debug;
 
-  const runJSParams = {
-    outputs: convertObject2Array(outputs),
-    env: {
-       executeSql: sql => env.executeSql(sql),
-       genUniqueId: env.genUniqueId,
-       getEntityName: isDebug ? _ => _ : env.getEntityName,
-    },
-  };
-  try {
-    if (runImmediate) {
-      if (env.runtime) {
+	const runJSParams = {
+		outputs: convertObject2Array(outputs),
+		env: {
+			executeSql: sql => env.executeSql(sql),
+			genUniqueId: env.genUniqueId,
+			getEntityName: isDebug ? _ => _ : env.getEntityName,
+			encrypt: env.encrypt,
+			decrypt: env.decrypt,
+		},
+	};
+	try {
+		if (runImmediate) {
+			if (env.runtime) {
 	      eval(decodeURIComponent(fns))(runJSParams);
-      }
-    }
-    inputs['input']((val) => {
+			}
+		}
+		inputs['input']((val) => {
 	    try {
 		    eval(decodeURIComponent(fns))({ ...runJSParams, inputs: convertObject2Array(val) });
-	    } catch (ex: any) {
+	    } catch (ex: AnyType) {
 		    onError?.(ex.message);
 		    env.edit ? console.error('js计算组件运行错误.', ex) : env.logger?.error(`${ex}`);
 	    }
-    });
-  } catch (ex: any) {
+		});
+	} catch (ex: AnyType) {
 	  onError?.(ex.message);
 	  env.edit ? console.error('js计算组件运行错误.', ex) : env.logger?.error(`${ex}`);
-  }
+	}
 }
